36ae58a201f0588d7eb38c6f467ad7618b82a4c6
Anilatta <95362042+Anilatta@users.noreply.github.com>
2022-09-29 18:56:37 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://astounding-wisp-096c68.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Talina Dzhavakhadze{" "}
-        </a>{" "}
-        and it is{" "}
-        <a
-          href="https://github.com/Anilatta/my-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://astounding-wisp-096c68.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Talina Dzhavakhadze{" "}
+          </a>{" "}
+          and it is{" "}
+          <a
+            href="https://github.com/Anilatta/my-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
