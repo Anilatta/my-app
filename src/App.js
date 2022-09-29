70e0f6d763c4b164675d695b8bca2f7d913da5df
Anilatta <95362042+Anilatta@users.noreply.github.com>
2022-09-29 18:37:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ export default function App() {
       <h1> Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://astounding-wisp-096c68.netlify.app/" target="_blank">
+        <a
+          href="https://astounding-wisp-096c68.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Talina Dzhavakhadze{" "}
         </a>{" "}
         and it is{" "}
-        <a href="https://github.com/Anilatta/my-app" target="_blank">
+        <a
+          href="https://github.com/Anilatta/my-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on GitHub
         </a>
       </footer>
