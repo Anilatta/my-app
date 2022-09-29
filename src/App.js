33c537c26994836bc285c7bd5466f60902dc657c
Anilatta <95362042+Anilatta@users.noreply.github.com>
2022-09-29 18:34:06 +0200

    Deleting logo and adding footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://astounding-wisp-096c68.netlify.app/" target="_blank">
+          Talina Dzhavakhadze{" "}
+        </a>{" "}
+        and it is{" "}
+        <a href="https://github.com/Anilatta/my-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
