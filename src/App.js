9f2c2fbac3c43c788762fe84e1e3df029109186b
Anilatta <95362042+Anilatta@users.noreply.github.com>
2022-09-29 18:35:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1> Weather App</h1>
       <footer>
         This project was coded by{" "}
         <a href="https://astounding-wisp-096c68.netlify.app/" target="_blank">
